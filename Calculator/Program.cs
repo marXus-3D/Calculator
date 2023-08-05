94b28a5502d5cdd4ea2da4f5bd51416f5dd5053d
Marcus Ayele <98558807+marXus-3D@users.noreply.github.com>
2023-08-05 21:57:12 +0300

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,6 +11,7 @@ namespace Calculator
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        //Nah this wildin
         [STAThread]
         static void Main()
         {
